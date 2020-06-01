6566b5a64072e1046cdd790aa7c465fc155fcd19
Elijah Penney <epenney@adt.com>
2020-06-01 09:39:11 -0700

    Added prettier

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// https://prettier.io/docs/en/options.html
+module.exports = {
+  trailingComma: 'all',
+  bracketSpacing: true,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'always',
+};
